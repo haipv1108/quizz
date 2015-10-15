0e95107991b04370027febe107a02bd9b03b6763
Nguyen Quoc Truong <truong.hn.1994@gmail.com>
2015-10-15 23:41:41 +0700

    create module test

---
+++ application/modules/createtest/models/mtest.php
@@ -0,0 +1,26 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Mtest extends CI_Model{
+	private $_name = 'test';
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
+
+	function count_all(){
+		return $this->db->count_all($this->_name);
+	}
+
+	function deletetest($id){
+		$this->db->where('id', $id)->limit(1)->delete($this->_name);
+	}
+
+	function deletequestion($testid){
+		$this->db->where('testid',$testid)->delete('question');
+	}
+
+	function insert_test($data) {
+		$str_query = $this->db->insert_string($data);
+		$this->db->query($str_query);
+	}
+}
