0b92485d8d301ee0d9c5d3a36067d013a82a1c4d
HaiLeader <pham.haisv@gmail.com>
2015-11-30 23:15:47 +0700

    create template test

--- application/modules/createtest/views/create_test1.php
+++ application/modules/createtest/views/create_test1.php
@@ -20,7 +20,7 @@
 			<div class="clear"></div>
 			<p>
 				<label>Mô tả</label>
-				<input class="text-input" type="text" name="test_des" value="<?php echo set_value('test_des'); ?>"/>
+				<textarea class="text-input textarea wysiwyg" name="test_des" cols="79" rows="3" ><?php echo set_value('test_des'); ?></textarea>
 			</p>
 			<div class="clear"></div>
 
@@ -47,7 +47,7 @@
 					<?php
 					foreach($categories as $key => $value) {
 						?>
-							<option value="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></option>
+							<option value="<?php echo $value['id']; ?>" <?php if(set_value('category') == $value['id']) echo 'selected';?>><?php echo $value['name']; ?></option>
 							<?php
 					 } ?>
 				</select>
@@ -72,7 +72,7 @@
 			<div class="clear"></div>
 			<p>
 				<label>Tổng số lượng câu hỏi</label>
-				<input type = text id = max_question name = max_question value="<?php echo set_value('max_question'); ?>">
+				<input class="text-input" type = text id = max_question name = max_question value="<?php echo set_value('max_question'); ?>">
 				<span id="avai"></span>
 			</p>
 			<div class="clear"></div>
@@ -91,27 +91,20 @@
 					}
 					?>
 				</select>
+				<p>
+					<label>Số lượng câu hỏi</label>
+					<input  class="text-input" type = text id = "num_question" value="<?php echo set_value('num_question'); ?>">
+					<span id="avai"></span>
+				</p>
+				<p>
+					<label>Điểm cho phần này</label>
+					<input class="text-input" type = text id = "score_question" value="<?php echo set_value('score_question'); ?>">
+					<span id="avai"></span>
+					<input class="button" type = button id = "btn_add" name = "btn_add" value="Add" />
+				</p>
 			</div>
 			</p>
 			<div class="clear"></div>
-			
-			<p>
-				<label>Số lượng câu hỏi</label>
-				<input type = text id = "num_question" value="<?php echo set_value('num_question'); ?>">
-				<span id="avai"></span>
-			</p>
-			<div class="clear"></div>
-			<p>
-				<label>Điểm cho phần này</label>
-				<input type = text id = "score_question" value="<?php echo set_value('score_question'); ?>">
-				<span id="avai"></span>
-			</p>
-			<div class="clear"></div>
-			
-			<p>
-				<input class="button" type = button id = "btn_add" name = "btn_add" value="Add" />
-			</p>
-			
 			
 			<div>
 				<label>Level</label>
@@ -123,8 +116,164 @@
 			</div>
 			<div class="clear"></div>
 			<p>
+				<table style = "width:100%">
+					<thead>
+					<tr>
+						<th>Subjects Selected</th>
+						<th>Number Of Question</th>
+						<th>Tool</th>
+					</tr>
+					</thead>
+
+					<tbody id = "sub_selected">
+
+					</tbody>
+
+				</table>
+			</p>
+			<p>
 				<input class="button" type="submit" name="submit" value="Submit" />
 			</p>
 		</form>
 	</div>
 </div>
+
+<script>
+    function include(arr, id) {
+        for (var i = 0; i < arr.length; ++i) {
+            if (arr[i]['id'] == id)
+                return true;
+        }
+    }
+
+    var subjectsSelected =[];
+    var currentNumQuestion = 0;
+
+    function updateView() {
+        var str = "";
+        var str1= "";
+        var subjectName, subjectID, numQuestion, scoreQuestion;
+
+        for (var i in subjectsSelected) {
+            if (subjectsSelected.hasOwnProperty(i)) {
+                subjectName = subjectsSelected[i]['name'];
+                subjectID = subjectsSelected[i]['id'];
+                numQuestion = subjectsSelected[i]['numQuestion'];
+                scoreQuestion = subjectsSelected[i]['scoreQuestion'];
+                level = subjectsSelected[i]['level'];
+                str = str +
+                    "<tr>" +
+                    "<td>"+subjectName+"</td>" +
+                    "<td>"+level+"</td>" + 
+                    "<td><input type = text id = text" + subjectID + " value=" +numQuestion +"></td>" +
+                    "<td>"+scoreQuestion+"</td>" + 
+                    "<td> <button type = 'button' onclick='updateSubject(" + subjectID + ")'>Update</button></td>" +
+                    "<td> <button type = 'button' onclick='removeSubject(" + subjectID + ")'>Remove</button></td>" +
+                    "</tr>";
+
+            }
+        }
+        document.getElementById("sub_selected").innerHTML = str;
+        document.getElementById("data").value = JSON.stringify(subjectsSelected);
+    }
+
+    function addSubject() {
+        var subjectName = $("#subject option:selected").text();
+        var subjectID = $('#subject').val();
+        var numQuestion = $('#num_question').val();
+        var subjectObject ={};
+        var maxQuestion = $('#max_question').val();
+        var scoreQuestion = $('#score_question').val();
+        var level = $('#level').val();
+
+        if (currentNumQuestion + parseInt(numQuestion) > maxQuestion) {
+            alert("Qua nhieu cau hoi trong phan hoc nay");
+            return;
+        }
+        if (subjectID == 'non_select' || $('#level').val() == 'non_select' || $('#category').val() == 'non_select') {
+            alert("Xin hãy chọn môn học, độ khó & phần học.");
+            return;
+        }
+        if (include(subjectsSelected, subjectID)) {
+            alert("Môn học đã tồn tại!");
+            return;
+        }
+        if (numQuestion < 1) {
+            alert("Xin hãy nhập số lượng câu hỏi");
+            return;
+        }
+        if (scoreQuestion <= 0) {
+            alert("Xin nhập điểm là một số lớn hơn 0");
+            return;
+        }
+
+        subjectObject['id'] = subjectID;
+        subjectObject['name'] = subjectName;
+        subjectObject['numQuestion'] = numQuestion;
+        subjectObject['scoreQuestion'] = scoreQuestion;
+        subjectObject['level'] = level;
+        subjectsSelected.push(subjectObject);
+        currentNumQuestion += parseInt(numQuestion);
+        updateView();
+    }
+
+
+    function updateSubject(id) {
+        var numQuestionNode = "text" + id;
+        var numQuestion = $(numQuestionNode).val();
+
+        for (var i in subjectsSelected) {
+            if (subjectsSelected.hasOwnProperty(i)) {
+                if (subjectsSelected[i]['id'] == id) {
+                    subjectsSelected[i]['numQuestion'] = numQuestion;
+                    alert("Update Success");
+                    return;
+                }
+            }
+        }
+    }
+
+    function removeSubject(id) {
+        for (var i in subjectsSelected) {
+            if (subjectsSelected.hasOwnProperty(i)) {
+                if (subjectsSelected[i]['id'] == id) {
+                    subjectsSelected.splice(i,1);
+                    updateView();
+                    return;
+                }
+            }
+        }
+    }
+        $(document).ready(function(){
+            $('#category').on('change', function(){
+                var selected = $('#category').val()
+                $.ajax({
+                    url: "<?php site_url();?>createtest/get_subject",
+                    type: 'POST',
+                    data: {cat_id : selected},
+                    dataType: 'html',
+                    success: function(response) {
+                        $('#subject').html(response);
+                    }
+                });
+            });
+
+
+            $('#category').on('change', function(){
+                var selected = $('#category').val()
+                $.ajax({
+                    url: "<?php site_url();?>createtest/get_level",
+                    type: 'POST',
+                    data: {cat_id : selected},
+                    dataType: 'html',
+                    success: function(response) {
+                        $('#level').html(response);
+                    }
+                });
+            });
+            
+            document.getElementById("btn_add").addEventListener('click',addSubject);
+
+        });
+
+    </script>
